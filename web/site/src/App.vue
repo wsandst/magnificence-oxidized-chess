f33c41c4d769793f3e0f95383846ac752be30f8c
wsandst <williamsandstrom99@hotmail.com>
2024-07-20 03:12:12 +0200

    Implement piece movement in web interface

--- web/site/src/App.vue
+++ web/site/src/App.vue
@@ -7,13 +7,20 @@ import PlayerInfo from './components/PlayerInfo.vue';
 import GameControls from './components/GameControls.vue';
 
 const currentPieces : any = ref(null);
+let engine : ChessEngine | null = null;
 
 onMounted(async () => {
     // Init wasm
     await init();
-    const engine = ChessEngine.new();
+    engine = ChessEngine.new();
     currentPieces.value = engine.get_pieces();
 });
+
+function onPieceMove({from, to}) {
+  engine?.make_move(from[0], from[1], to[0], to[1]);
+  currentPieces.value = engine?.get_pieces();
+}
+
 </script>
 
 <template>
@@ -22,7 +29,7 @@ onMounted(async () => {
         <div class="flex flex-col justify-center items-center gap-4 mx-auto w-[min(500px,100vw)] pt-32">
             <div class="w-full px-6 flex flex-col gap-3">
               <PlayerInfo image="src/assets/images/robot-profile.png" name="Magnificence"/>
-              <Board :pieces="currentPieces"/>
+              <Board :pieces="currentPieces" @piece-moved="onPieceMove"/>
               <div class="flex flex-row justify-between">
                 <PlayerInfo image="src/assets/images/human-profile.png" name="Human"/>
                 <GameControls/>
--- web/site/src/components/Board.vue
+++ web/site/src/components/Board.vue
@@ -3,9 +3,11 @@
 
 import { ref, onMounted } from 'vue';
 
-const boardElement = ref(null);
-const refreshPieces = ref(0);
-const movingPiece = ref(null);
+const emit = defineEmits(['pieceMoved']);
+
+const boardElement : any = ref(null);
+const refreshPieces : any = ref(0);
+const movingPiece : any = ref(null);
 
 function boardPositionModulo(row : number, col: number) : number {
     return (row * 8 + col + row) % 2;
@@ -18,7 +20,7 @@ const props = defineProps({
     },
 })
 
-const pieceToIconMap = {
+const pieceToIconMap : any = {
     1: "src/assets/icons/pieces/white-pawn.svg",
     2: "src/assets/icons/pieces/white-bishop.svg",
     3: "src/assets/icons/pieces/white-knight.svg",
@@ -45,26 +47,37 @@ function calculateTranslationBasedMousePosition(x : number, y: number) {
 
     let xPos = x - rect.left - (0.125 / 2) * boardElement.value.clientWidth;
     let yPos = y - rect.top - (0.125 / 2) * boardElement.value.clientHeight;
-    console.log(xPos, yPos);
     return `transform: translate(${xPos}px, ${yPos}px);`
 }
 
 
-function pieceDragStartMouse(e: MouseEvent, x: number, y: number) {
-    console.log(e, x, y);
+function pieceDragStart(e: any) {
     movingPiece.value = e.target;
 }
 
-function pieceDragStopMouse(e: MouseEvent, x: number, y: number) {
-    movingPiece.value = null;
-}
-
-function pieceDragStartTouch(e: TouchEvent, x: number, y: number) {
-    console.log(e, x, y);
-    movingPiece.value = e.target;
-}
-
-function pieceDragStopTouch(e: TouchEvent, x: number, y: number) {
+function pieceDragStop(e: any, x: number, y: number) {
+    var rect = boardElement.value.getBoundingClientRect();
+    let dragStopX = null;
+    let dragStopY = null;
+    let squareSizePx = 0.125 * boardElement.value.clientWidth;
+    if (e.type == "touchend") {
+        dragStopX = e.touches[0].pageX;
+        dragStopY = e.touches[0].pageY;
+    }
+    else if (e.type == "mouseup") {
+        dragStopX = e.x;
+        dragStopY = e.y;
+    }
+    if (movingPiece.value != null) {
+        let to_x = Math.floor((dragStopX - rect.left) / squareSizePx);
+        let to_y = Math.floor((dragStopY - rect.top) / squareSizePx);
+        if (to_x >= 0 && to_x < 8 && to_y >= 0 && to_y < 8) {
+            emit("pieceMoved", {from: [x, y], to: [to_x, to_y]})
+        }
+        else {
+            movingPiece.value.style = calculateTranslationBasedOnPosition(x, y);
+        }
+    }
     movingPiece.value = null;
 }
 
@@ -95,20 +108,20 @@ onMounted(() => {
         <div class="flex flex-row w-full h-[12.5%]" v-for="row in 8" :key="row">
             <div class="w-[12.5%] h-full" 
                 :class="{ 'bg-dark-square': boardPositionModulo(row, col) == 1, 'bg-light-square': boardPositionModulo(row, col) == 0}"
-                v-for="col in 8" :key="row * col">
+                v-for="col in 8" :key="row * 8 + col">
             </div>
         </div>
         <div class="absolute w-full" :key="refreshPieces" v-if="refreshPieces != 0">
             <img 
-                v-for="{x, y, piece} in pieces" :key="x * y + 'b'"
+                v-for="{x, y, piece} in pieces" :key="y * 8 + x + 'b'"
                 class="w-[12.5%] object-cover absolute select-none cursor-pointer" 
                 :src="pieceToIconMap[piece]"
                 :style="calculateTranslationBasedOnPosition(x, y)"
                 draggable="false"
-                @mousedown="(e) => pieceDragStartMouse(e, x, y)"
-                @touchstart="(e) => pieceDragStartTouch(e, x, y)"
-                @mouseup="(e) => pieceDragStopMouse(e, x, y)"
-                @touchend="(e) => pieceDragStartTouch(e, x, y)"
+                @mousedown="pieceDragStart"
+                @touchstart="pieceDragStart"
+                @mouseup="(e) => pieceDragStop(e, x, y)"
+                @touchend="(e) => pieceDragStop(e, x, y)"
             />
         </div>
     </div>
